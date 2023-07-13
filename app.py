49069e0e112a1670ca2a33a0b9c23b2ecb29aa65
Thelepan-kl <136567811+Thelepan-kl@users.noreply.github.com>
2023-07-13 16:11:09 +0530

    Update app.py
    from "flask import "is changed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
+from flask import Flask,request,app,jsonify,url_for,render_template
 import json
 import pickle
-
-from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
