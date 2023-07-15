13d07315f5b5f8eca36b0fe9f60cc722a02a9f55
Thelepan-kl <thelepankl@gmail.com>
2023-07-15 20:47:57 +0530

    add all the css files

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def predict():
     final_input = np.concatenate((scaled_input, unscaled_data), axis=1)
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("index.html",prediction_text="The premium price  is {}".format(output))
+    return render_template("index.html",prediction_text="YOUR PREMIUM PRICE IS {}".format(output))
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080)
